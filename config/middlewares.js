45794a5337c02d9c29ffc9a68fbeff994a3651ec
jesus.lopez <jesus.lopez@lasamericas.com.co>
2024-03-04 18:14:58 -0500

    node modules

---
+++ config/middlewares.js
@@ -0,0 +1,26 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "conect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null
+        }
+      }
+    }
+  },
+];
+
